9c3cdea2e23587ee37d3d79dca054bb03bf5bf4b
MatWich <wicherskimateusz@gmail.com>
2021-03-02 22:22:25 +0100

    background issue fixed

--- main.py
+++ main.py
@@ -2,28 +2,26 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-'''
-@app.route('/get', methods=['GET'])
-def index():
-    return render_template('index.html')
-'''
 
 @app.route('/', methods=['GET', 'POST'])
 def my_post_form():
     if request.method == 'POST':
-        data = [request.form['text0'], request.form['text1'], request.form['text2'], request.form['text3']]
+        dataForTable = [request.form['text0'], request.form['text1'], request.form['text2'], request.form['text3']]
         
+        ''' Do uzupelnienia o te cale obliczenia'''
+        """totalGain to niby jest cos tam minus cos tam"""
+        
+        cos_tam1 = 10
+        cos_tam2 = 20
+        totalGain = cos_tam2 - cos_tam1 # i ta zmienna tez trzeba
+        dataForLabels = [cos_tam1, cos_tam2, totalGain]
 
-        return render_template('results.html', data=data)
+        # Musimy przekazac 4 elementowa tablice
+        return render_template('results.html', dataForTable=dataForTable, dataForLabels=dataForLabels)
     else:
         return render_template('index.html')
 
 
-
-@app.route('/hi/<username>')
-def greet(username):
-    return f"Hi there, {username}"
-
-
 if __name__ == '__main__':
     app.run(debug=True)
+    
