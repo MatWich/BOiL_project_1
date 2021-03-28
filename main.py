eb8cbc0b4025d032d696cb33ff10344a1420f3d3
MatWich <wicherskimateusz@gmail.com>
2021-03-28 20:13:06 +0200

    frontend modified to meet the criteria getting data from labels enabled

--- main.py
+++ main.py
@@ -7,16 +7,30 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def my_post_form():
     if request.method == 'POST':
-        dataForTable = [request.form['text0'], request.form['text1'], request.form['text2'], request.form['text3']]
-        
-        ''' Do uzupelnienia o te cale obliczenia'''
-        """totalGain to niby jest cos tam minus cos tam"""
-        dane = {"koszty_trans_p1": [3, 8],
-                    "koszty_trans_p2": [4, 2],
-                    "podaz": [35, 15],
-                    "popyt": [20, 30],
-                    "koszty_zakupu": [15, 10],
-                    "cena_sprzedazy": [30, 22]}
+        koszty_trans_p1 = [int(request.form['text0']), int(request.form['text1'])]
+        koszty_trans_p2 = [int(request.form['text2']), int(request.form['text3'])]
+
+        podaz = [int(request.form['podaz1']), int(request.form['podaz2'])]
+        popyt = [int(request.form['popyt1']), int(request.form['popyt2'])]
+
+        koszty_zakupu = [int(request.form['koszt1']), int(request.form['koszt2'])]
+        cena_sprzedazy = [int(request.form['cena1']), int(request.form['cena2'])]
+
+        dane = {"koszty_trans_p1": koszty_trans_p1,
+                    "koszty_trans_p2": koszty_trans_p2,
+                    "podaz": podaz,
+                    "popyt": popyt,
+                    "koszty_zakupu": koszty_zakupu,
+                    "cena_sprzedazy": cena_sprzedazy}
+
+        """ zakomentowac jesli juz wszystko dziala  """
+        # dane = {"koszty_trans_p1": [3, 8],
+        #             "koszty_trans_p2": [4, 2],
+        #             "podaz": [35, 15],
+        #             "popyt": [20, 30],
+        #             "koszty_zakupu": [15, 10],
+        #             "cena_sprzedazy": [30, 22]}
+        """ koniec obszaru do zakomentowania """
 
         op = Optymalizacja(dane)
         op.set_up()
